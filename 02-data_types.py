a558e5a27cfd7f32319623a15a86a70c4e9d7f7b
Hesflay <hesflay@outlook.com>
2023-03-16 22:36:01 +0300

    .
    New files added.

---
+++ 02-data_types.py
@@ -0,0 +1,26 @@
+# str
+print("Hesflay")
+
+# int
+print(20)
+
+# float
+print(20.4)
+
+# list
+["banana", "apple", "cherry"]
+
+# tupple 
+("banana", "apple", "cherry")
+
+# range
+range(6)
+
+# dict
+{"modelName": "volvo", "year": 2023}
+
+# set
+{"banana", "apple", "cherry"}
+
+# bool
+True
