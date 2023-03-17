0c84e7676698c79cfb46f88bf56ca6856d1cfa65
Hesflay <hesflay@outlook.com>
2023-03-17 12:25:23 +0300

    .

---
+++ 06-while_for.py
@@ -0,0 +1,28 @@
+# while loop
+
+i = 1
+while i <= 6:
+    print("While Loop :", i)
+    i += 1
+
+# for loop
+
+for x in range(6):
+    print("For Loop: ", x)
+
+
+# contune - break
+
+# contiune
+for x in range(1, 5):
+    if x == 4:
+        continue
+    print("contiune: ",x)
+
+
+# break
+
+for y in range(1,5):
+    if y == 4:
+        break
+    print("break: ",y)
