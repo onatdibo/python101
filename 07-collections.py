c4f5efd8471f61017058f9e289d06affc8b95b0a
Hesflay <hesflay@outlook.com>
2023-03-17 16:20:20 +0300

    .

--- 07-collections.py
+++ 07-collections.py
@@ -1,4 +1,4 @@
-# list
+# 1- LIST
 
 products = ["notebook", "book", "pencil"]
 print(products)
@@ -22,3 +22,21 @@ print(products)
 
 products[2] = "keyboard" # Old : "keyboard"
 print(products)
+
+
+# 2- TUPPLES
+
+fruits = ("apple", "banana", "cherry")
+print(fruits)
+
+print(len(fruits)) # To determine how many items a tuple has, use the len() function:
+
+# access the tupple
+print(fruits[1])
+
+test = (1, True, "Hf", 2.4) #  tupples different data types
+print(test)
+
+
+test2 = tuple(("john", "adriana")) # created new tupple
+print(test2)
