c0b215d52da1e8025728b42d9d01e888794b200e
Hesflay <hesflay@outlook.com>
2023-03-17 13:54:57 +0300

    .

--- 07-collections.py
+++ 07-collections.py
@@ -1,1 +1,24 @@
-# coming soon...
+# list
+
+products = ["notebook", "book", "pencil"]
+print(products)
+
+# list items
+
+print(products[2]) # output : "pencil"
+
+# Changeable list
+
+products.append("bag") # added new product
+print(products)
+
+# remove list
+
+products.remove("bag") # removed item "bag"
+print(products)
+
+
+# change the prudcts 
+
+products[2] = "keyboard" # Old : "keyboard"
+print(products)
