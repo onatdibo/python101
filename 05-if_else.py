cd3ac03a97100ddbe39cbe0923a4ef47564f4aa1
Hesflay <hesflay@outlook.com>
2023-03-17 08:42:53 +0300

    Update 05-if_else.py

--- 05-if_else.py
+++ 05-if_else.py
@@ -1,1 +1,13 @@
-# coming soon...
+x = 30
+y = 10
+
+if x > y:
+    print("Big")
+elif x == y:
+    print("equal")
+else:
+    print("Small")
+
+# Short Hand If ... Else
+
+print("Big") if x > y else print("Small")
